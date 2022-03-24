2129698d3cf42da5f397999b4ae6b8d253fd32a5
NgocMy1806 <83016185+NgocMy1806@users.noreply.github.com>
2022-03-24 22:42:32 +0700

    update bubble sort

--- bubble sort.php
+++ bubble sort.php
@@ -1,22 +1,33 @@
 <?php
+// Your code here!
 $a= array(5,2,1,4,3);
 $i=0;
-function swap($a[$i],$a[$i+1]){
-    $i=0;
-    $z=$a[$i];
-    $a[$i]=$a[$i+1];
-    $a[$i+1]=$z; 
+function swap (&$m,&$n){
+    $z=$m;
+    $m =$n;
+    $n=$z; 
 };
-while($i<4){
-    if ($a[$i] < $a[$i+1]){
-        echo "vị trí của ".$a[$i]." và". $a[$i+1] ."đã đúng";
-        ;
-    } else {
-        echo "vị trí của ".$a[$i]." và". $a[$i+1] ."ko đúng";
-        swap ($a[$i],$a[$i+1])
+
+for ($b=count($a); $b>=1; $b--){
+    for ($i=0;$i<$b-1;$i++){
+        if ($a[$i] < $a[$i+1]){
+            echo "vị trí của ".$a[$i]." và". $a[$i+1] ."đã đúng";
+          
+        } else {
+            echo "vị trí của ".$a[$i]." và". $a[$i+1] ."ko đúng";
+            $x=&$a[$i];
+            $y=&$a[$i+1];
+            echo $x. $y."\n";
+            swap ($x,$y);
+            echo $x. $y;
+            
+        }
+        echo "<pre>";
+        print_r($a);
+        echo "</pre>";
+
     }
-       ;
-       $i++;}
 
+}
 
-?>
+?> 
