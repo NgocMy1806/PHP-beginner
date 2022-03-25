dc6ce291c8ae693bf28b8e1f74795ef367a49145
NgocMy1806 <83016185+NgocMy1806@users.noreply.github.com>
2022-03-25 13:39:08 +0700

    format

--- SwapInClass.php
+++ SwapInClass.php
@@ -1,38 +1,33 @@
 <?php
-// Your code here!
-class algo {
-function swap (&$m,&$n){
-    $z=$m;
-    $m =$n;
-    $n=$z; 
-}
-}
-$algo1= new algo();
-
-$a= array(5,2,1,4,3);
-$i=0;
-for ($b=count($a); $b>=1; $b--){
-    for ($i=0;$i<$b-1;$i++){
-        if ($a[$i] < $a[$i+1]){
-            echo "vị trí của ".$a[$i]." và ". $a[$i+1] ." đã đúng"."\n";
-          
-        } else {
-            echo "vị trí của ".$a[$i]." và ". $a[$i+1] ." ko đúng"."\n";
-            $x=&$a[$i];
-            $y=&$a[$i+1];
-            echo $x. $y;
-            $algo1 -> swap ($x,$y);
-            echo " swap to ". $x. $y."\n";
-            
-        }
-       
-        print_r($a);
-       
-
+class algo
+{
+    function swap(&$m, &$n)
+    {
+        $z = $m;
+        $m = $n;
+        $n = $z;
     }
-
 }
+$algo1 = new algo();
 
+$a = [5, 2, 1, 4, 3];
+$i = 0;
+for ($b = count($a); $b >= 1; $b--) {
+    for ($i = 0; $i < $b - 1; $i++) {
+        if ($a[$i] < $a[$i + 1]) {
+            echo "vị trí của " .$a[$i] ." và " .$a[$i + 1] ." đã đúng" . "\n";
+        } else {
+            echo "vị trí của " .$a[$i] ." và " .$a[$i + 1] ." ko đúng" ."\n";
+            $x = &$a[$i];
+            $y = &$a[$i + 1];
+            echo $x . $y;
+            $algo1->swap($x, $y);
+            echo " swap to " . $x . $y . "\n";
+        }
+
+        print_r($a);
+    }
+}
 ?> 
 
 
