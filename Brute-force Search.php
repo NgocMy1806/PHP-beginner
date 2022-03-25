1c2e8db8613180e28b3121fa5bc0c9fcda40ae0b
NgocMy1806 <83016185+NgocMy1806@users.noreply.github.com>
2022-03-25 13:43:19 +0700

    format

--- Brute-force Search.php
+++ Brute-force Search.php
@@ -1,13 +1,14 @@
 <?php
 //Brute-force Search: find the element with value 4
 
-$a= array(5,1,4,2,3);
+$a = [5, 1, 4, 2, 3];
 
-foreach($a as $x=>$value){
-        if ($value==4){
-        $z=$x+1;
+foreach ($a as $x => $value) {
+    if ($value == 4) {
+        $z = $x + 1;
         echo "value 4 is the $z element of a, with index is $x";
-    break;}
-    echo $value ."\n";//e thêm dòng này để e biết được là đã chạy loop mấy lần rồi, cái break có active ko
-  }
+        break;
+    }
+    echo $value . "\n"; //e thêm dòng này để e biết được là đã chạy loop mấy lần rồi, cái break có active ko
+}
 ?>
